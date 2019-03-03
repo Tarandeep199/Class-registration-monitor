01c8ea7cb9a02d49789365a68f37669995baa7a2
Tarandeep <tarandeepsingh199@gmail.com>
2019-03-02 18:22:57 -0800

    Update Search3.py

--- Search3.py
+++ Search3.py
@@ -2,9 +2,14 @@ import requests
 from bs4 import BeautifulSoup as bs
 import time
 from datetime import datetime
+from twilio.rest import Client
 
 
 def main3():
+    print("main3 has been started")
+    account_sid = 'AC0a4967cd787a6ff9d833d778af5a82f2'
+    auth_token = '4ca2e51948b023457d9dec3c3bad830c'
+    client = Client(account_sid, auth_token)
     crnNumber = None
     allSeats = []
     round1 = []
@@ -41,11 +46,15 @@ def main3():
         allNumbers = []
         crnNumbers = []
         seatsAvailable = []
+        classNames = []
         #print(list)
         #print(list)
 
 
         for i in range(len(list)):
+            if len(list[i])>4:
+                if list[i][3] == "-" or list[i][4] == "-" and list[i][1]!=":" and list[i][2]!=":":
+                    classNames.append(list[i])
             if list[i].isdigit()==True:
                 if int(list[i]) < 500:
                     allNumbers.append(list[i])
@@ -68,12 +77,31 @@ def main3():
         time.sleep(1)
 
         if round1 != round2:
-            if counter2>1:
-                for i in range(len(round1)):
-                    if(round1[i]!=round2[i]):
-                        print(str(datetime.now().time().strftime('%I:%M %p')) + " The number of seats for class number #" + crnNumbers[i] + " has changed by " + str(int(round1[i])-int(round2[i])))
+                if counter2>1:
+                            for i in range(len(round1)):
+                                if(round1[i]!=round2[i]):
+                                    if counter2%2==0:
+                                        result = int(round1[i]) - int(round2[i])
+                                    else:
+                                        result = int(round2[i]) - int(round1[i])
+                                    if result == 1:
+                                        print("dropped spot")
+                                        print(str(datetime.now().time().strftime('%I:%M %p'))+ " " + classNames[i] + "(" + crnNumbers[i] + ") has just opened up with " + seatsAvailable[i] + " available space(s)")
+                                        message = client.messages \
+                                                        .create(
+                                                             body=(str(datetime.now().time().strftime('%I:%M %p'))+ " " + classNames[i] + "(" + crnNumbers[i] + ") has just opened up with " + seatsAvailable[i] + " available space(s)"),
+                                                             from_='+19497993437',
+                                                             to='+19713257738'
+                                                         )
+                                    else:
+                                        print(str(datetime.now().time().strftime('%I:%M %p'))+ " " + classNames[i] + "(" + crnNumbers[i] + ") has taken a spot, there are " + seatsAvailable[i] + " available space(s)")
 
 
+        #print(list)
+        #print(len(classNames))
+        #print(classNames)
+        #print(len(seatsAvailable))
+        #print(seatsAvailable)
         counter2 = counter2+1
         #print("checkpoint")
     #print(allNumbers)
