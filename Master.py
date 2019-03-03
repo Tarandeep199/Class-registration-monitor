04780446f8558e047ea685d094868e60fe73ef00
Tarandeep <tarandeepsingh199@gmail.com>
2019-03-02 18:20:32 -0800

    Update Master.py

--- Master.py
+++ Master.py
@@ -6,9 +6,12 @@ from Search2 import main2
 from Search3 import main3
 from datetime import datetime
 
-if __name__ == '__main__':
+
+counter = 0
+
+if __name__ == '__main__'  and counter<5:
     Thread(target = main1).start()
     Thread(target = main2).start()
     Thread(target = main3).start()
-
+    counter = counter+1
 print("Up and running!")
